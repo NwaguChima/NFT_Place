9e88724a186492c86740f8857b9f8a2f942d99bf
nwagu chima <chima.nwagu@decagon.dev>
2022-04-30 20:06:19 +0100

    toggle button functionality implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// element toggle
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// navbar variables
+const navbar = document.querySelector("[data-navbar]");
+const navbarToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+
+navbarToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(navbar);
+});
