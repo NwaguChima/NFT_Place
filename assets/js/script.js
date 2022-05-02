ceee561d584ffdfc943ea473de97db29e715b277
nwagu chima <chima.nwagu@decagon.dev>
2022-05-02 18:26:54 +0100

    go to top implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,14 @@ for (let i = 0; i < whishListBtn.length; i++) {
     elemToggleFunc(this);
   });
 }
+
+// Go to top variable
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+});
